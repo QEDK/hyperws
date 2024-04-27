4b73d0cb835a64558c26b86a03f19d0b28415aa3
qedk <1994constant@gmail.com>
2024-04-27 20:49:00 +0400

    feat: implement feature completeness

--- index.ts
+++ index.ts
@@ -1,19 +1,17 @@
+'use strict'
 import { createClient } from 'redis'
 import { nanoid } from 'nanoid'
 import { type ServerWebSocket } from 'bun'
 
 const client = await createClient({ url: 'redis://localhost:6379' })
-  .on('error', err => console.log('Redis Client Error', err))
+  .on('error', err => console.error('Unable to connect to DB!', err))
   .connect()
 
-const socket = new WebSocket('wss://lc-rpc-goldberg.avail.tools:443/ws')
-
+let socket = new WebSocket('wss://avail-turing.public.blastapi.io')
 interface WS extends ServerWebSocket<unknown> {
-  data: WSData
-}
-
-interface WSData {
-  id: string
+  data: {
+    id: string
+  }
 }
 
 interface JSONRPCResponse extends MessageEvent<any> {
@@ -30,64 +28,117 @@ const server = Bun.serve({
     if (server.upgrade(req)) {
       return // do not return a Response
     }
-    return new Response('Upgrade failed :(', { status: 500 })
+    return new Response('❌ Upgrade failed', { status: 500 })
   },
   websocket: {
     async message (ws: WS, message) {
       const body = JSON.parse(message as string)
-      console.log({ requestBody: body })
-      console.log({ stringified: JSON.stringify(body) })
+      if (body.method === 'chain_subscribeFinalizedHeads') {
+        ws.send(JSON.stringify({
+          jsonrpc: '2.0',
+          id: body.id,
+          result: await client.get('chain_subscribeFinalizedHeads')
+        }), true)
+
+        return
+      } else if (body.method === 'grandpa_subscribeJustifications') {
+        ws.send(JSON.stringify({
+          jsonrpc: '2.0',
+          id: body.id,
+          result: await client.get('grandpa_subscribeJustifications')
+        }), true)
+        return
+      }
       const requestId = body.id
-      console.log({ connId1: ws.data.id, requestId1: body.id })
-      let ret = await client.hGet(ws.data.id.toString(), body.id.toString())
       body.id = String(ws.data.id) + String(body.id)
-      console.log({ ret })
-      if (ret === null) {
-        console.log({ id: ws.data.id })
-        socket.send(JSON.stringify(body))
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        socket.addEventListener('message', async (event): Promise<void> => {
-          const body: JSONRPCResponse = JSON.parse(event.data as string)
-          if (body.method === 'chain_subscribeFinalizedHeads') {
-            ws.publish('chain_subscribeFinalizedHeads', body.params[0] as string)
-            return
-          }
-          console.log({ body })
-          const connId = body.id.toString().slice(0, 21)
-          const requestId = parseInt(body.id.toString().slice(21, body.id.toString().length))
-          body.id = requestId
-          console.log({ connId })
-          console.log({ requestId })
-          console.log({ eventData: event.data })
-          await client.hSet(connId, requestId, JSON.stringify(body))
-        })
-        while (await client.hGet(ws.data.id.toString(), requestId.toString()) === null) {
-          await new Promise((resolve) => setTimeout(resolve, 1000))
-        }
-        ret = await client.hGet(ws.data.id.toString(), requestId.toString())
-        console.log({ responseBody: ret })
-        ws.send(ret as string)
+      let ret = await client.get(body.id)
+      const cachedRequest = structuredClone(body)
+      delete cachedRequest.jsonrpc
+      delete cachedRequest.id
+      const cachedResponse = await client.get(JSON.stringify(cachedRequest))
+      if (cachedResponse !== null) {
+        const cachedResponseObj = JSON.parse(cachedResponse)
+        cachedResponseObj.id = requestId
+        ws.send(JSON.stringify(cachedResponseObj), true)
+        return
       } else {
-        console.log({ resposeBody: ret })
-        ws.send(ret as string)
+        await client.set(`${body.id as string}-cache`, JSON.stringify(cachedRequest), { EX: 20 })
+      }
+      if (ret === null) {
+        socket.send(JSON.stringify(body))
+        while (ret === null) {
+          await new Promise((resolve) => setTimeout(resolve, 1000))
+          ret = await client.get(String(body.id))
+        }
+        const returnObj = JSON.parse(ret)
+        returnObj.id = requestId
+        ws.send(JSON.stringify(returnObj), true)
+      } else {
+        const returnObj = JSON.parse(ret)
+        returnObj.id = requestId
+        ws.send(JSON.stringify(returnObj), true)
       }
     }, // a message is received
     async open (ws) {
       ws.data = {
         id: nanoid()
       }
-      console.log('open')
+      ws.subscribe('chain_finalizedHead')
+      ws.subscribe('grandpa_justifications')
     }, // a socket is opened
-    async close (_ws, _code, _message) {
-      console.log('close')
-    }, // a socket is closed
-    async drain (_ws) {
-      console.log('drain')
-    }, // the socket is ready to receive more data,
     perMessageDeflate: true,
     idleTimeout: 60,
     maxPayloadLength: 1024 * 1024
   } // handlers
 })
+let finalizedHeadId: string
+let grandpaId: string
+socket.onclose = () => {
+  socket = new WebSocket('wss://turing-testnet.avail-rpc.com')
+}
+socket.onopen = () => {
+  finalizedHeadId = nanoid()
+  grandpaId = nanoid()
+  socket.send(JSON.stringify({
+    jsonrpc: '2.0',
+    id: finalizedHeadId,
+    method: 'chain_subscribeFinalizedHeads'
+  }))
+  socket.send(JSON.stringify({
+    jsonrpc: '2.0',
+    id: grandpaId,
+    method: 'grandpa_subscribeJustifications'
+  }))
+}
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+socket.addEventListener('message', async (event): Promise<void> => {
+  const body: JSONRPCResponse = JSON.parse(event.data as string)
+  if (body === null || body === undefined) {
+    return
+  }
+  if (body.id === finalizedHeadId) {
+    await client.set('chain_subscribeFinalizedHeads', String(body.result))
+    return
+  } else if (body.id === grandpaId) {
+    await client.set('grandpa_subscribeJustifications', String(body.result))
+    return
+  }
+  if (body.method === 'chain_finalizedHead' && body.params !== undefined) {
+    server.publish('chain_finalizedHead', JSON.stringify(body), true)
+    return
+  }
+  if (body.method === 'grandpa_justifications' && body.params !== undefined) {
+    server.publish('grandpa_justifications', JSON.stringify(body), true)
+    return
+  }
+  if (body.id === null || body.id === undefined) {
+    return
+  }
+  await client.set(String(body.id), JSON.stringify(body), { EX: 20 })
+  const cachedRequest = await client.get(`${body.id}-cache`)
+  if (cachedRequest !== null) {
+    await client.set(cachedRequest, JSON.stringify(body), { EX: 20 })
+  }
+})
 
-console.log(`🦻 Listening on ${server.hostname}:${server.port}`)
+console.log(`✨ Starting hyperws on ${server.hostname}:${server.port}`)
